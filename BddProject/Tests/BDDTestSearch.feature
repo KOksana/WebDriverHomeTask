a544a4ed00f1c9be8093b9c91d58ea0fe0405152
Oksana Koval <Oksana_Koval@epam.com>
2020-01-30 11:34:32 +0200

    Add bdd scenarios

--- BddProject/Tests/BDDTestSearch.feature
+++ BddProject/Tests/BDDTestSearch.feature
@@ -1,7 +1,17 @@
 ﻿Feature: BDDTestSearch
 
-@mytag
 Scenario: Check search product
 	Given I am on home page
 	When I search for 'macbook'
 	Then All results items contain 'macbook' in title
+
+Scenario: Search and check product details
+	Given I am on home page
+	When I search for 'TV'
+	And I navigate to first product item
+	Then Product info is the same as on search page
+
+Scenario: Search product and check filter list
+	Given I am on home page
+	When I search for 'TV'
+	Then I check filter list
--- BddProject/Tests/BDDTestSearchDefinition.cs
+++ BddProject/Tests/BDDTestSearchDefinition.cs
@@ -6,19 +6,13 @@ using WebDriverHomeTask.Steps;
 namespace BddProject.Steps
 {
     [Binding]
-    public class BDDTestSearchDefinition : TechTalk.SpecFlow.Steps
+    public class BDDTestSearchDefinition : BaseSteps
     {
         protected const string URL = "https://www.citrus.ua/";
-
-        //private static string searchProductMac = "macbook";
-        private static string catalogItem = "Телевизоры, фото, видео";
-        private static string itemSpecific = "LG";
-        private static string searchProductTv = "TV";
 
         private readonly BaseSteps _baseSteps;
         private readonly HomePageSteps _homePageSteps;
         private readonly SearchResultPageSteps _searchResultPageSteps;
-        private readonly ProductListPageSteps _productListPageSteps;
         private readonly ProductPageSteps _productPageSteps;
 
         public BDDTestSearchDefinition()
@@ -26,7 +20,6 @@ namespace BddProject.Steps
             _baseSteps = new BaseSteps();
             _homePageSteps = new HomePageSteps();
             _searchResultPageSteps = new SearchResultPageSteps();
-            _productListPageSteps = new ProductListPageSteps();
             _productPageSteps = new ProductPageSteps();
         }
 
@@ -36,7 +29,6 @@ namespace BddProject.Steps
         {
             _baseSteps.NavigateToUrl(URL);
             _homePageSteps.WaitPageIsDisplayed();
-           // ScenarioContext.Current.Pending();
         }
         
         [When(@"I search for '(.*)'")]
@@ -45,10 +37,22 @@ namespace BddProject.Steps
             _homePageSteps.Search(searchProduct);
 
             _searchResultPageSteps.WaitSearchPageIsDisplayed();
-
-          //  ScenarioContext.Current.Pending();
         }
-        
+
+        [When(@"I navigate to first product item")]
+        public void WhenINavigateToFirstProductItem()
+        {
+            var expectedName = _searchResultPageSteps.GetProductName(0);
+            var expectedPrice = _searchResultPageSteps.GetProductPrice(0);
+
+            ScenarioContext.Current.Add("product_expectedName", expectedName);
+            ScenarioContext.Current.Add("product_expectedPrice", expectedPrice);
+
+            _searchResultPageSteps.NavigateToProduct(0);
+
+            _productPageSteps.WaitProductPageIsDisplayed();
+        }
+
         [Then(@"All results items contain '(.*)' in title")]
         public void ThenAllResultsItemsContainSearchProductInTitle(string searchProduct)
         {
@@ -57,7 +61,38 @@ namespace BddProject.Steps
             Assert.IsTrue(resultList.Length > 0, "No elements in result");
 
             Assert.IsTrue(resultList.All(i => i.ToLower().Contains(searchProduct)), "Not All contains product name");
-           // ScenarioContext.Current.Pending();
+        }
+
+        
+        [Then(@"Product info is the same as on search page")]
+        public void ThenProductInfoIsTheSameAsOnSearchPage()
+        {
+
+            var expectedName = ScenarioContext.Current.Get<string>("product_expectedName");
+            var expectedPrice = ScenarioContext.Current.Get<string>("product_expectedPrice");
+
+            var actualName = _productPageSteps.GetProductTitle;
+            var actualPrice = _productPageSteps.GetProductPrice;
+
+            Assert.AreEqual(expectedName, actualName, "Product name is different");
+            Assert.AreEqual(expectedPrice, actualPrice, "Product price is different");
+        }
+
+        [Then(@"I check filter list")]
+        public void ThenICheckFilterList()
+        {
+            var expectedFilterList = new string[] {
+                "Смартфоны",
+                "Мобильные телефоны",
+                "Планшеты",
+                "Ноутбуки и ультрабуки",
+                "iMac"
+            };
+
+            var actualFilterList = _searchResultPageSteps.GetFilterListForTV;
+
+            Assert.AreEqual(expectedFilterList.Length, actualFilterList.Length, "Filter list has different length");
+            CollectionAssert.AreEquivalent(expectedFilterList, actualFilterList, "Different filter element name");
         }
     }
 }
